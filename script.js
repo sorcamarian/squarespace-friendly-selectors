6f102c2ab2d6d4760fd9dc9d8536f06802622e2a
Sorca Marian <sorca.marian@gmail.com>
2020-09-10 18:01:43 +0300

    Add Classes to SECTION and Entire PAGE

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function(){
   var hasLoadedInControlPannel =  document.body.className.indexOf("sqs-edit-mode") > 0;
-  
+
 
 
   if(!hasLoadedInControlPannel){
@@ -87,6 +87,79 @@
 
 
 
+        // Make elements more CSS friendly - Class to Parent-SECTION - Squarespace
+        // Make elements more CSS friendly - Class to Parent-SECTION - Squarespace
+        // Make elements more CSS friendly - Class to Parent-SECTION - Squarespace
+        // Make elements more CSS friendly - Class to Parent-SECTION - Squarespace
+        // Make elements more CSS friendly - Class to Parent-SECTION - Squarespace
+        // TODO
+        (function() {
+          var classToParentSectionElements    = document.getElementsByClassName("css-helpers-class-to-parent-section-element"),
+            i = 0,
+            classToParentSectionElementsLen = classToParentSectionElements.length;
+
+            // loop through classToParentSectionElements
+            for (i ; i < classToParentSectionElementsLen; i++){
+              // debugger;
+              var element                      = classToParentSectionElements[i],
+                  classForParentParentElement  = element.getAttribute("data-class-to-parent-section-element"),
+                  elementSection               = element.parentNode.parentNode,
+                  parentParentElementSection   = (function () {
+                      var parent             = element.parentNode,
+                      isNotSectionNode       = parent.nodeName !== "SECTION",
+                      isNotIndexSectionClass = !(parent.className.indexOf("index-section") > -1 && parent.id);
+                      console.log(parent);
+                      // debugger;
+                      // TODO - Improve checking for while()
+                      // var not updating during while
+                      // maybe use something else - another approach?
+                      var canLoopRun = isNotSectionNode;
+                      if( canLoopRun ) {
+                            canLoopRun = isNotIndexSectionClass;
+                      }
+                      while(canLoopRun) {
+                        parent = parent.parentNode;
+                        isNotSectionNode       = parent.nodeName !== "SECTION",
+                        isNotIndexSectionClass = !(parent.className.indexOf("index-section") > -1 && parent.id);
+                        canLoopRun = isNotSectionNode;
+                        if( canLoopRun ) {
+                          canLoopRun = isNotIndexSectionClass;
+                        }
+                      }
+                      return parent;
+                  })();
+
+              elementSection.setAttribute("id", "class-to-parent-section-element-" + classForParentParentElement);
+              parentParentElementSection.className += " " + classForParentParentElement;
+            }
+        }) ();
+        // END: Make elements more CSS friendly - Class to Parent-SECTION - Squarespace
+
+
+
+
+
+        // Make elements more CSS friendly - Class to <body> - Squarespace
+        // Make elements more CSS friendly - Class to <body> - Squarespace
+        // Make elements more CSS friendly - Class to <body> - Squarespace
+        // Make elements more CSS friendly - Class to <body> - Squarespace
+        // Make elements more CSS friendly - Class to <body> - Squarespace
+        (function() {
+          // HTML example: <div id="sqs-fs_add-page-classes" data-classes="about-page"></div>
+          "use strict";
+          var addPageClasses = document.querySelector("#sqs-fs_add-page-classes");
+          if(addPageClasses) {
+              var classesForBody    = addPageClasses.getAttribute("data-classes")
+                                        .split(" "),
+                  classesForBodyLen = classesForBody.length;
+              for (var idx = 0; idx < classesForBodyLen; idx++) {
+                document.body.className += (" " + classesForBody[idx]);
+              }
+          }
+        })();
+        // END: Make elements more CSS friendly - Class to <body> - Squarespace
+
+
 
   } // if(!hasLoadedInControlPannel)
-})()
+})();
